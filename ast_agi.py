83dcb9c5d90e53cfb2581d7ca8c53fdea6c040ea
Tatianno Ferreira Alves <tatianno.alves@mmtelecomm.com.br>
2020-04-09 12:09:51 -0300

    Subindo audios

--- ast_agi.py
+++ ast_agi.py
@@ -7,16 +7,7 @@ agi = AGI()
 #funcoes
 #Logica CPF
 
-def gravacao_CPF():
-	agi.stream_file(proj_folder + '/informe_cpf')
-	agi.stream_file('beep')
-	agi.appexec('Read','CPF1')
-	CPF = agi.get_variable('CPF1')
-	result = {}
 
-ANI = agi.env['agi_callerid']
-agi.set_variable('ANI',ANI)
-agi.verbose("Origem: %s" % ANI)
 agi.stream_file(proj_folder + '/bem_vindo')	
 sys.exit()
 
