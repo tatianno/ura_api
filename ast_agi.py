c8612b263537628abbe071c5207fb610867d8b64
Tatianno Ferreira Alves <tatianno.alves@mmtelecomm.com.br>
2020-04-09 12:56:17 -0300

    Inicio de testes

--- ast_agi.py
+++ ast_agi.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-#import requests
+import requests
 import json
 from asterisk.agi import *
 from settings import proj_folder
 
 #Criando objetos
 agi = AGI()
-'''
+
 #funcoes
 def get_api(CPF):
     url = 'http://54.94.135.76:82/autoteste/pabx.asp?cpf=' + str(CPF)
     r = requests.get(url=url)
     agi.verbose("Resultado GET: %s" % r)
-'''
+
 #Logica CPF
 ANI = agi.env['agi_callerid']
 agi.set_variable('_ANI',ANI)
@@ -23,4 +23,5 @@ agi.stream_file(proj_folder + '/audios/cpf_conv')
 agi.stream_file('beep')
 agi.appexec('Read','_CPF')
 CPF = agi.get_variable('CPF')
+get_api(CPF)
 sys.exit()
